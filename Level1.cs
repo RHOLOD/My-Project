1a5949ce7cb06134fcbc75cb55d5cf1bfcfb1a90
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2021-04-13 12:29:59 +0500

    Update Level1.cs

--- Level1.cs
+++ Level1.cs
@@ -13,7 +13,10 @@ namespace Level1Space
             int l = oksana.Length;
             for (int i = 1; i < l; i += 2)
             {
-                s = s + oksana[i] * oksana[i - 1];
+                    if (i == 1)
+                        s = s + oksana[i] * (oksana[i - 1]);
+                    else
+                        s = s + oksana[i] * (oksana[i - 1]- oksana[i - 2]);
             }
 
             return s;
