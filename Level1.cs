66bbcde99722091d9e77dd0969a49887def7baa6
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2021-04-13 10:38:40 +0500

    Add files via upload

--- Level1.cs
+++ Level1.cs
@@ -5,23 +5,18 @@ using System.Text;
 
 namespace Level1Space
 {
-    public static class Level1
+    class Level1
     {
-        public static int squirrel(int N)
+        public static int odometer(int[] oksana)
         {
-            int a = 1;
-            for (int i = 1; i <= N; i++)
+            int s = 0;
+            int l = oksana.Length;
+            for (int i = 1; i < l; i += 2)
             {
-                a = a * (int)i;
-            }
-            if (a > 10)
-            {
-                string s = a.ToString();
-                s = s.Remove(1);
-                a = Convert.ToInt32(s);
+                s = s + oksana[i] * oksana[i - 1];
             }
 
-            return a;
+            return s;
         }
     }
 }
