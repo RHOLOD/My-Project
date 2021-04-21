ca35a80fafc14fbcbecb706bb88f8c0cdcae339e
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2021-04-21 15:34:31 +0500

    Add files via upload

---
+++ Level1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Level1Space
+{
+    public static class Level1
+    {
+        public static int SumOfThe(int N, int[] data)
+        {
+            int[] massiv = new int[N];
+            massiv = data;
+            int rezult = 0;
+            for (int i = 0; i < N; i++)
+            {
+                int nil = 0;
+                for (int j = 0; j < N; j++)
+                {
+                    nil = nil + massiv[j];
+                }
+                nil = nil - massiv[i];
+                if (nil == massiv[i])
+                {
+                    rezult = massiv[i];
+                    break;
+                }
+            }
+
+            return rezult;
+        }
+    }
+}
