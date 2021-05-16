98cd241ccde7dae0c9a8621077ce3ba43352eef3
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2021-05-16 15:01:29 +0500

    Add files via upload

---
+++ Level1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Level1Space
+{
+    public static class Level1
+    {
+        public static bool LineAnalysis(string line)
+        {
+            if (line.Length == 1)
+            {
+                return true;
+            }
+            bool proverka = false;
+            int two = 0;
+            for (int i = 1; i < line.Length; i++)
+            {
+                if (line[i] == '*')
+                {
+                    two = i;
+                    break;
+                }
+            }
+            string pat = line.Substring(1, two);
+            int kol = two;
+            if (kol == 0)
+            {
+                kol = 1;
+            }
+            for (int i = 1; i < line.Length - kol + 1; i += kol)
+            {
+                string str2 = line.Substring(i, kol);
+                if (str2 == pat)
+                {
+                    proverka = true;
+                }
+                else
+                {
+                    proverka = false;
+                }
+            }
+
+            return proverka;
+        }
+    }
+}
