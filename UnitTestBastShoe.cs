f262b9789e18b116d2618ae885a94e209b86e81d
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2021-05-21 21:16:24 +0500

    Add files via upload

---
+++ UnitTestBastShoe.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Level1Space;
+namespace UnitTestLevel1Space
+{
+    [TestClass]
+    public class UnitTestBastShoe
+    {
+        [TestMethod]
+        public void BastShoe()
+        {
+            Level1 level = new Level1();
+            string expected = "Привет";
+            string actual = level.BastShoe("1 Привет");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!";
+            actual = level.BastShoe("1 , Мир!");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!++";
+            actual = level.BastShoe("1 ++");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!";
+            actual = level.BastShoe("2 2");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!++";
+            actual = level.BastShoe("4");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!";
+            actual = level.BastShoe("4");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!*";
+            actual = level.BastShoe("1 *");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!";
+            actual = level.BastShoe("4");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!";
+            actual = level.BastShoe("4");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!";
+            actual = level.BastShoe("4");
+            Assert.AreEqual(expected, actual);
+            expected = ",";
+            actual = level.BastShoe("3 6");
+            Assert.AreEqual(expected, actual);
+            expected = "";
+            actual = level.BastShoe("2 100");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет";
+            actual = level.BastShoe("1 Привет");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!";
+            actual = level.BastShoe("1 , Мир!");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!++";
+            actual = level.BastShoe("1 ++");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!";
+            actual = level.BastShoe("4");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет";
+            actual = level.BastShoe("4");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!";
+            actual = level.BastShoe("5");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет";
+            actual = level.BastShoe("4");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!";
+            actual = level.BastShoe("5");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!++";
+            actual = level.BastShoe("5");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!++";
+            actual = level.BastShoe("5");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!++";
+            actual = level.BastShoe("5");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет, Мир!";
+            actual = level.BastShoe("4");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет";
+            actual = level.BastShoe("4");
+            Assert.AreEqual(expected, actual);
+            expected = "Прив";
+            actual = level.BastShoe("2 2");
+            Assert.AreEqual(expected, actual);
+            expected = "Привет";
+            actual = level.BastShoe("4");
+            Assert.AreEqual(expected, actual);
+            expected = "Прив";
+            actual = level.BastShoe("5");
+            Assert.AreEqual(expected, actual);
+            expected = "Прив";
+            actual = level.BastShoe("5");
+            Assert.AreEqual(expected, actual);
+            expected = "Прив";
+            actual = level.BastShoe("5");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
