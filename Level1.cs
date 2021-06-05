ebb079242084ed87cad109a73b0657e52bdc3beb
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2021-06-06 01:14:52 +0500

    Add files via upload

---
+++ Level1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Level1Space
+{
+    public static class Level1
+    {
+        public static string[] TreeOfLife(int H, int W, int N, string[] tree)
+        {
+            string[] str = new string[5] { "1", "1", "1", "1", "1" };
+            return str;
+        }
+    }
+}
