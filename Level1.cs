c26ef0a0d09ea5d72d5394542187a986a7af6aa0
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2021-06-20 12:12:45 +0500

    Add files via upload

---
+++ Level1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Level1Space
+{
+    public static class Level1
+    {
+        public static bool TransformTransform(int[] A, int N)
+        {
+            return true;
+        }
+    }
+}
