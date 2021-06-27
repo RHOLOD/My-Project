fc177199c3e5f18493677e154fe86b6fdbd7d44a
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2021-06-27 22:24:18 +0500

    Add files via upload

---
+++ Level1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Level1Space
+{
+    public static class Level1
+    {
+        public static bool white_walkers(string village)
+        {
+            return true;
+        }
+    }
+}
